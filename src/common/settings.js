edb425eb2051da29de5c161dbda2d613ded75e08
moonstar-x <christian.lopez99@outlook.com>
2020-09-17 23:16:23 -0500

    Corrected settings and added default values

--- src/common/settings.js
+++ src/common/settings.js
@@ -1,6 +1,6 @@
 import config from '../../config/settings.json';
 
 export const discordToken = process.env.DISCORD_TOKEN || config.discord_token;
-export const prefix = process.env.PREFIX || config.prefix;
-export const ownerID = process.env.OWNER_ID || config.owner_id;
-export const inviteURL = process.env.INVITE_URL || config.invite_url;
+export const prefix = process.env.PREFIX || config.prefix || '$';
+export const ownerID = process.env.OWNER_ID || config.owner_id || null;
+export const inviteURL = process.env.INVITE_URL || config.invite_url || null;
