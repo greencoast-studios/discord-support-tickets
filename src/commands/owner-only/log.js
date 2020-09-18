7a2d7724230e6e07da2108c02c64d164985693d8
moonstar-x <christian.lopez99@outlook.com>
2020-09-18 15:49:19 -0500

    Created log command.

---
+++ src/commands/owner-only/log.js
@@ -0,0 +1,54 @@
+import { Command } from 'discord.js-commando';
+import { guildSettingKeys } from '../../common/constants';
+
+class LogCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'log',
+      memberName: 'log',
+      group: 'owner-only',
+      description: 'Enable or disable the logging of support channels.',
+      examples: [`${client.commandPrefix}log enable`, `${client.commandPrefix}log disable`],
+      guildOnly: false,
+      argsType: 'multiple'
+    });
+  }
+
+  handleEnable(message) {
+    const previousValue = this.client.provider.get(message.guild, guildSettingKeys.log);
+
+    if (previousValue) {
+      message.reply('Logging is already enabled.');
+      return;
+    }
+
+    this.client.provider.set(message.guild, guildSettingKeys.log, true);
+    message.reply("Logging is now enabled. Chat log files will be saved in the bot's data folder.");
+  }
+
+  handleDisable(message) {
+    const previousValue = this.client.provider.get(message.guild, guildSettingKeys.log);
+
+    if (!previousValue) {
+      message.reply('Logging is already disabled.');
+      return;
+    }
+
+    this.client.provider.set(message.guild, guildSettingKeys.log, false);
+    message.reply('Logging is now disabled. Chat logs will not be saved.');
+  }
+
+  run(message, [option]) {
+    if (option === 'enable') {
+      return this.handleEnable(message);
+    }
+
+    if (option === 'disable') {
+      return this.handleDisable(message);
+    }
+
+    message.reply('Invalid argument, please use either: **enable** or **disable**.');
+  }
+}
+
+export default LogCommand;
---
+++ test/commands/owner-only/log.spec.js
@@ -0,0 +1,97 @@
+import LogCommand from '../../../src/commands/owner-only/log';
+
+let command;
+
+const clientMock = {
+  commandPrefix: '$',
+  provider: {
+    set: jest.fn(),
+    get: jest.fn()
+  }
+};
+
+const messageMock = {
+  reply: jest.fn(),
+  guild: 'guild'
+};
+
+describe('Commands - Log', () => {
+  afterEach(() => {
+    messageMock.reply.mockClear();
+    clientMock.provider.get.mockClear();
+    clientMock.provider.set.mockClear();
+  });
+
+  describe('Arg: No args', () => {
+    beforeEach(() => {
+      command = new LogCommand(clientMock);
+      command.run(messageMock, []);
+    });
+
+    it('should call message.reply once.', () => {
+      expect(messageMock.reply.mock.calls.length).toBe(1);
+    });
+
+    it('should reply with the invalid argument message.', () => {
+      expect(messageMock.reply.mock.calls[0][0]).toBe('Invalid argument, please use either: **enable** or **disable**.');
+    });
+  });
+
+  describe('Arg: enable', () => {
+    it('should reply with the already enabled message if already enabled.', () => {
+      clientMock.provider.get.mockImplementation(() => true);
+      command = new LogCommand(clientMock);
+      command.run(messageMock, ['enable']);
+
+      expect(messageMock.reply.mock.calls.length).toBe(1);
+      expect(messageMock.reply.mock.calls[0][0]).toBe('Logging is already enabled.');
+    });
+
+    it('should set the log setting to true.', () => {
+      clientMock.provider.get.mockImplementation(() => false);
+      command = new LogCommand(clientMock);
+      command.run(messageMock, ['enable']);
+      
+      expect(clientMock.provider.set.mock.calls.length).toBe(1);
+      expect(clientMock.provider.set.mock.calls[0][2]).toBe(true);
+    });
+
+    it('should reply with the enabled message if enabled.', () => {
+      clientMock.provider.get.mockImplementation(() => false);
+      command = new LogCommand(clientMock);
+      command.run(messageMock, ['enable']);
+
+      expect(messageMock.reply.mock.calls.length).toBe(1);
+      expect(messageMock.reply.mock.calls[0][0]).toBe("Logging is now enabled. Chat log files will be saved in the bot's data folder.");
+    });
+  });
+
+  describe('Arg: disable', () => {
+    it('should reply with the already disabled message if already disabled.', () => {
+      clientMock.provider.get.mockImplementation(() => false);
+      command = new LogCommand(clientMock);
+      command.run(messageMock, ['disable']);
+
+      expect(messageMock.reply.mock.calls.length).toBe(1);
+      expect(messageMock.reply.mock.calls[0][0]).toBe('Logging is already disabled.');
+    });
+
+    it('should set the log setting to false.', () => {
+      clientMock.provider.get.mockImplementation(() => true);
+      command = new LogCommand(clientMock);
+      command.run(messageMock, ['disable']);
+
+      expect(clientMock.provider.set.mock.calls.length).toBe(1);
+      expect(clientMock.provider.set.mock.calls[0][2]).toBe(false);
+    });
+
+    it('should reply with the disabled message if disabled.', () => {
+      clientMock.provider.get.mockImplementation(() => true);
+      command = new LogCommand(clientMock);
+      command.run(messageMock, ['disable']);
+      
+      expect(messageMock.reply.mock.calls.length).toBe(1);
+      expect(messageMock.reply.mock.calls[0][0]).toBe('Logging is now disabled. Chat logs will not be saved.');
+    });
+  });
+});
