4fd3ae4964140072574f4dbe949601b1ee852f91
moonstar-x <christian.lopez99@outlook.com>
2020-09-17 23:26:41 -0500

    Corrected how settings are loaded

--- src/common/settings.js
+++ src/common/settings.js
@@ -1,6 +1,10 @@
-import config from '../../config/settings.json';
+import fs from 'fs';
+import path from 'path';
 
-export const discordToken = process.env.DISCORD_TOKEN || config.discord_token;
-export const prefix = process.env.PREFIX || config.prefix || '$';
-export const ownerID = process.env.OWNER_ID || config.owner_id || null;
-export const inviteURL = process.env.INVITE_URL || config.invite_url || null;
+const configFilePath = path.join(__dirname, '../../config/settings.json');
+const configFromFile = fs.existsSync(configFilePath) ? JSON.parse(fs.readFileSync(configFilePath)) : {};
+
+export const discordToken = process.env.DISCORD_TOKEN || configFromFile.discord_token;
+export const prefix = process.env.PREFIX || configFromFile.prefix || '$';
+export const ownerID = process.env.OWNER_ID || configFromFile.owner_id || null;
+export const inviteURL = process.env.INVITE_URL || configFromFile.invite_url || null;
