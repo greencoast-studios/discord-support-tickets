471795e378c75f6b3f85cb7ee9e24a7beafa7807
moonstar-x <christian.lopez99@outlook.com>
2020-09-18 01:17:17 -0500

    Added a default value to discordToken and added a test file for the settings.

--- src/common/settings.js
+++ src/common/settings.js
@@ -4,7 +4,7 @@ import path from 'path';
 const configFilePath = path.join(__dirname, '../../config/settings.json');
 const configFromFile = fs.existsSync(configFilePath) ? JSON.parse(fs.readFileSync(configFilePath)) : {};
 
-export const discordToken = process.env.DISCORD_TOKEN || configFromFile.discord_token;
+export const discordToken = process.env.DISCORD_TOKEN || configFromFile.discord_token || null;
 export const prefix = process.env.PREFIX || configFromFile.prefix || '$';
 export const ownerID = process.env.OWNER_ID || configFromFile.owner_id || null;
 export const inviteURL = process.env.INVITE_URL || configFromFile.invite_url || null;
