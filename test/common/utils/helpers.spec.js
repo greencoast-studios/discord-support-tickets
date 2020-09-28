65694638c0a9d5ee2f475ba9abecd9e41de8a3c7
moonstar-x <christian.lopez99@outlook.com>
2020-09-27 20:26:33 -0500

    Created a channel serializer

--- test/common/utils/helpers.spec.js
+++ test/common/utils/helpers.spec.js
@@ -1,4 +1,4 @@
-import { parseMention, isThisTheDiscordError } from '../../../src/common/utils/helpers';
+import { parseMention, isThisTheDiscordError, serializeChannel } from '../../../src/common/utils/helpers';
 import { messageMock, channelMock } from '../../../__mocks__/discordMocks';
 import { discordErrors } from '../../../src/common/constants';
 
@@ -52,6 +52,34 @@ describe('Utils - Helpers', () => {
       error.name = discordErrors.unknownMessage.name;
       error.message = discordErrors.unknownMessage.message;
       expect(isThisTheDiscordError(error, discordErrors.unknownMessage)).toBe(true);
+    });
+  });
+
+  describe('serializeChannel()', () => {
+    it('should return a Promise.', () => {
+      expect(serializeChannel(channelMock)).toBeInstanceOf(Promise);
+    });
+
+    it('should resolve a string.', () => {
+      return serializeChannel(channelMock)
+        .then((result) => {
+          expect(typeof result).toBe('string');
+        });
+    });
+
+    it('should resolve a correctly formatted string.', () => {
+      return serializeChannel(channelMock)
+        .then((result) => {
+          const lines = result.split('\n');
+          expect(lines[0]).toBe('---#########---');
+          expect(lines[1]).toBe(`Channel: ${channelMock.name}`);
+          expect(lines[2]).toBe(`Channel ID: ${channelMock.id}`);
+          expect(lines[3]).toBe(`Category Name: ${channelMock.parent.name}`);
+          expect(lines[4]).toBe(`Guild: ${channelMock.guild.name}`);
+          // 5 and 6 are skipped because I can't mock Date.toLocaleString :(
+          expect(lines[7]).toBe(`Members: ${channelMock.members.map((m) => m.displayName).join(', ')}`);
+          expect(lines[8]).toBe('---#########---');
+        });
     });
   });
 });
